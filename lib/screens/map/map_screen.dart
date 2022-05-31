5ad9555c8ad3da8ba5fb1b4a4d73c53df3cdeae8
Haze-I <selamsileshi57@gmail.com>
2022-05-31 14:02:00 +0300

    feat: added map screen

--- lib/screens/map/map_screen.dart
+++ lib/screens/map/map_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:dio/dio.dart';
 import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
@@ -11,54 +10,65 @@ class MapScreen extends StatefulWidget {
         9.0123,
         38.7626,
       ),
-      zoom: 12.0);
+      zoom: 13.0);
 
   @override
   State<MapScreen> createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
-  late GoogleMapController _googleMapController;
-
   final Marker _origin = Marker(
     markerId: const MarkerId('origin'),
     infoWindow: const InfoWindow(title: 'Origin'),
     icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
     position: const LatLng(9.0123, 38.7626),
   );
+  late GoogleMapController _googleMapController;
 
-  Future getLocation() async {
+  Future<Position> getLocation() async {
+    await Geolocator.requestPermission();
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.low);
+    return position;
 
-    final Marker _destination = Marker(
-        markerId: const MarkerId('destination'),
-        infoWindow: const InfoWindow(title: 'Destination'),
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-        position: LatLng(position.latitude, position.longitude));
     //late Dio _info;
   }
 
-  //Marker dest = _destination;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   getLocation();
-  // }
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        myLocationButtonEnabled: false,
-        zoomControlsEnabled: false,
-        initialCameraPosition: MapScreen._initialCameraPosition,
-        onMapCreated: (controller) => _googleMapController = controller,
-        markers: {
-          _origin,
-        },
-      ),
+    return FutureBuilder<Position>(
+        future: getLocation(),
+        builder: (context, snapshot) {
+          return Scaffold(
+            body: GoogleMap(
+              myLocationButtonEnabled: false,
+              zoomControlsEnabled: false,
+              initialCameraPosition: MapScreen._initialCameraPosition,
+              onMapCreated: (controller) => _googleMapController = controller,
+              markers: {
+                if (snapshot.hasData &&
+                    snapshot.connectionState == ConnectionState.done)
+                  _getDestinationMarker(snapshot.data!),
+                _origin,
+              },
+            ),
+          );
+        });
+  }
+
+  Marker _getDestinationMarker(Position position) {
+    final Marker destinationMarker = Marker(
+      markerId: const MarkerId('destination'),
+      infoWindow: const InfoWindow(title: 'Destination'),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+      position: LatLng(position.latitude, position.longitude),
     );
+
+    return destinationMarker;
   }
 }
